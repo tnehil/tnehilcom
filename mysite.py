6962e8ef57457853da0dadbf66b6d906b55bc053
Tom Nehil <tom.nehil@gmail.com>
2014-03-08 17:12:39 -0600

    Changed to a bootstrap template for fun and profit

--- mysite.py
+++ mysite.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('temphome.html')
+    return render_template('basicone.html')
 
 if __name__ == "__main__":
     app.run()
